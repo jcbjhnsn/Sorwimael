8bad2f598ad2a47995174620aa03e230174eabd2
Jacob Johnson <julietalphakiloecho@gmail.com>
2012-01-06 15:11:34 -0600

    trying

--- raise-to-power/raise-to-power.rkt
+++ raise-to-power/raise-to-power.rkt
@@ -4,4 +4,20 @@
 
 (print test)
 
+;Goal:
+; (raise-to-power b e)
+; where 'b' is the base and 'e' is the exponent.
+; So, take 'b' and multiply it by 'b' 'e' number of times. 
+; For exmaple if passed '2 3' 'raise-to-power' should return 64
+; (raise-to-power 2 3)
+;   (* 
+;    (* 2 2)
+;    (* 2 2)
+;    (* 2 2)
+;    )
+
+(define
+  exp 
+     (+ n n)
+  )
 
